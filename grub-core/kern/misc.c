2a71d2434b01daaafa64cd340ea441def459d880
Paulo Flabiano Smorigo <pfsmorigo@br.ibm.com>
2012-11-27 16:58:39 -0200

    Add %X option to printf functions.

--- grub-core/kern/misc.c
+++ grub-core/kern/misc.c
@@ -596,7 +596,7 @@ grub_divmod64 (grub_uint64_t n, grub_uint64_t d, grub_uint64_t *r)
 static char *
 grub_lltoa (char *str, int c, unsigned long long n)
 {
-  unsigned base = (c == 'x') ? 16 : 10;
+  unsigned base = ((c == 'x') || (c == 'X')) ? 16 : 10;
   char *p;
 
   if ((long long) n < 0 && c == 'd')
@@ -611,7 +611,7 @@ grub_lltoa (char *str, int c, unsigned long long n)
     do
       {
 	unsigned d = (unsigned) (n & 0xf);
-	*p++ = (d > 9) ? d + 'a' - 10 : d + '0';
+	*p++ = (d > 9) ? d + ((c == 'x') ? 'a' : 'A') - 10 : d + '0';
       }
     while (n >>= 4);
   else
@@ -702,6 +702,7 @@ grub_vsnprintf_real (char *str, grub_size_t max_len, const char *fmt0, va_list a
 	{
 	case 'p':
 	case 'x':
+	case 'X':
 	case 'u':
 	case 'd':
 	case 'c':
@@ -780,6 +781,7 @@ grub_vsnprintf_real (char *str, grub_size_t max_len, const char *fmt0, va_list a
       switch (c)
 	{
 	case 'x':
+	case 'X':
 	case 'u':
 	case 'd':
 	  if (longlongfmt)
@@ -921,6 +923,7 @@ grub_vsnprintf_real (char *str, grub_size_t max_len, const char *fmt0, va_list a
 	  longlongfmt |= (sizeof (void *) == sizeof (long long));
 	  /* Fall through. */
 	case 'x':
+	case 'X':
 	case 'u':
 	  unsig = 1;
 	  /* Fall through. */
