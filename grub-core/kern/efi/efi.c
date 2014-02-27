cbadbfbf9694d91b7c5d15f1d7ac7b9773168bab
Fedora Ninjas <grub2-owner@fedoraproject.org>
2014-02-26 21:49:12 -0500

    always return error to UEFI

--- grub-core/kern/efi/efi.c
+++ grub-core/kern/efi/efi.c
@@ -159,7 +159,7 @@ grub_exit (void)
 {
   grub_machine_fini (GRUB_LOADER_FLAG_NORETURN);
   efi_call_4 (grub_efi_system_table->boot_services->exit,
-              grub_efi_image_handle, GRUB_EFI_SUCCESS, 0, 0);
+              grub_efi_image_handle, GRUB_EFI_LOAD_ERROR, 0, 0);
   for (;;) ;
 }
 
