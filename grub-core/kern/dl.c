68728a66b415d124864e84db66944750d6e8c42f
Peter Jones <pjones@redhat.com>
2015-10-06 16:09:25 -0400

    Make any of the loaders that link in efi mode honor secure boot.
    And in this case "honor" means "even if somebody does link this in, they
    won't register commands if SB is enabled."
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- grub-core/kern/dl.c
+++ grub-core/kern/dl.c
@@ -32,6 +32,7 @@
 #include <grub/env.h>
 #include <grub/cache.h>
 #include <grub/i18n.h>
+#include <grub/efi/sb.h>
 
 /* Platforms where modules are in a readonly area of memory.  */
 #if defined(GRUB_MACHINE_QEMU)
@@ -704,7 +705,7 @@ grub_dl_load_file (const char *filename)
   grub_dl_t mod = 0;
 
 #ifdef GRUB_MACHINE_EFI
-  if (grub_efi_secure_boot ())
+  if (grub_efi_get_secureboot () == GRUB_EFI_SECUREBOOT_MODE_ENABLED)
     {
 #if 0
       /* This is an error, but grub2-mkconfig still generates a pile of
