a28fc78d531ea309d59d085ce89bcc292f35774e
Peter Jones <pjones@redhat.com>
2015-10-06 16:09:25 -0400

    Make any of the loaders that link in efi mode honor secure boot.
    And in this case "honor" means "even if somebody does link this in, they
    won't register commands if SB is enabled."
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- grub-core/loader/i386/linux.c
+++ grub-core/loader/i386/linux.c
@@ -35,6 +35,7 @@
 #include <grub/i18n.h>
 #include <grub/lib/cmdline.h>
 #include <grub/linux.h>
+#include <grub/efi/sb.h>
 
 GRUB_MOD_LICENSE ("GPLv3+");
 
@@ -1137,6 +1138,9 @@ static grub_command_t cmd_linux, cmd_initrd;
 
 GRUB_MOD_INIT(linux)
 {
+  if (grub_efi_secure_boot())
+    return;
+
   cmd_linux = grub_register_command ("linux", grub_cmd_linux,
 				     0, N_("Load Linux."));
   cmd_initrd = grub_register_command ("initrd", grub_cmd_initrd,
@@ -1146,6 +1150,9 @@ GRUB_MOD_INIT(linux)
 
 GRUB_MOD_FINI(linux)
 {
+  if (grub_efi_secure_boot())
+    return;
+
   grub_unregister_command (cmd_linux);
   grub_unregister_command (cmd_initrd);
 }
