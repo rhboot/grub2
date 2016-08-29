22d26153c1d92b8a42d1ec823b7f7c77c1cc0a8c
Lenny Szubowicz <lszubowi@redhat.com>
2016-08-29 11:04:48 -0400

    Normalize slashes in tftp paths.
    Some tftp servers do not handle multiple consecutive slashes correctly;
    this patch avoids sending tftp requests with non-normalized paths.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- grub-core/net/tftp.c
+++ grub-core/net/tftp.c
@@ -300,6 +300,25 @@ destroy_pq (tftp_data_t data)
   grub_priority_queue_destroy (data->pq);
 }
 
+/* Create a normalized copy of the filename.
+   Compress any string of consecutive forward slashes to a single forward
+   slash. */
+static void
+grub_normalize_filename (char *normalized, const char *filename)
+{
+  char *dest = normalized;
+  char *src = filename;
+
+  while (*src != '\0')
+    {
+      if (src[0] == '/' && src[1] == '/')
+	src++;
+      else
+	*dest++ = *src++;
+    }
+  *dest = '\0';
+}
+
 static grub_err_t
 tftp_open (struct grub_file *file, const char *filename)
 {
@@ -337,7 +356,10 @@ tftp_open (struct grub_file *file, const char *filename)
   rrqlen = 0;
 
   tftph->opcode = grub_cpu_to_be16_compile_time (TFTP_RRQ);
-  grub_strcpy (rrq, filename);
+
+  /* Copy and normalize the filename to work-around issues on some tftp
+     servers when file names are being matched for remapping. */
+  grub_normalize_filename (rrq, filename);
   rrqlen += grub_strlen (filename) + 1;
   rrq += grub_strlen (filename) + 1;
 
