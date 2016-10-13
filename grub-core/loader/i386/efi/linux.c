34d717eb9b7a818a179017cb396df545a19c9462
Matthew Garrett <mjg59@coreos.com>
2016-10-13 13:55:26 -0700

    Make TPM errors less fatal
    Handle TPM errors, and stop trying to use the TPM once we hit one.

--- grub-core/loader/i386/efi/linux.c
+++ grub-core/loader/i386/efi/linux.c
@@ -133,6 +133,7 @@ grub_cmd_initrd (grub_command_t cmd __attribute__ ((unused)),
           goto fail;
         }
       grub_tpm_measure (ptr, cursize, GRUB_BINARY_PCR, "grub_linuxefi", "Initrd");
+      grub_print_error();
       ptr += cursize;
       grub_memset (ptr, 0, ALIGN_UP_OVERHEAD (cursize, 4));
       ptr += ALIGN_UP_OVERHEAD (cursize, 4);
@@ -198,6 +199,7 @@ grub_cmd_linux (grub_command_t cmd __attribute__ ((unused)),
     }
 
   grub_tpm_measure (kernel, filelen, GRUB_BINARY_PCR, "grub_linuxefi", "Kernel");
+  grub_print_error();
 
   rc = grub_linuxefi_secure_validate (kernel, filelen);
   if (rc < 0)
