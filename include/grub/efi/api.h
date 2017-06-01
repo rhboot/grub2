51302209b6ad66435dd0ab6aa112258e3abe9e8d
Peter Jones <pjones@redhat.com>
2017-06-01 10:07:50 -0400

    Add PRIxGRUB_EFI_STATUS and use it.
    This avoids syntax checkers getting confused about if it's llx or lx.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- include/grub/efi/api.h
+++ include/grub/efi/api.h
@@ -527,6 +527,14 @@ typedef grub_uint8_t grub_efi_char8_t;
 typedef grub_uint16_t grub_efi_char16_t;
 
 typedef grub_efi_intn_t grub_efi_status_t;
+/* Make grub_efi_status_t reasonably printable. */
+#if GRUB_CPU_SIZEOF_VOID_P == 8
+#define PRIxGRUB_EFI_STATUS "lx"
+#define PRIdGRUB_EFI_STATUS "ld"
+#else
+#define PRIxGRUB_EFI_STATUS "llx"
+#define PRIdGRUB_EFI_STATUS "lld"
+#endif
 
 #define GRUB_EFI_ERROR_CODE(value)	\
   ((((grub_efi_status_t) 1) << (sizeof (grub_efi_status_t) * 8 - 1)) | (value))
