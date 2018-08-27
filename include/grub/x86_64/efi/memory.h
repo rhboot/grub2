bf3218fb90b3a6130c98f9ca41a49f12fefdefda
Peter Jones <pjones@redhat.com>
2018-08-27 14:31:37 -0400

    Fix GRUB_EFI_MAX_USABLE_ADDRESS to be 64-bit on x86_64 EFI machines.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- include/grub/x86_64/efi/memory.h
+++ include/grub/x86_64/efi/memory.h
@@ -2,9 +2,9 @@
 #include <grub/efi/memory.h>
 
 #if defined (__code_model_large__)
-#define GRUB_EFI_MAX_USABLE_ADDRESS 0xffffffff
+#define GRUB_EFI_MAX_USABLE_ADDRESS 0xffffffffffffffffULL
 #else
-#define GRUB_EFI_MAX_USABLE_ADDRESS 0x7fffffff
+#define GRUB_EFI_MAX_USABLE_ADDRESS 0x7fffffffffffffffULL
 #endif
 
 #endif /* ! GRUB_MEMORY_CPU_HEADER */
