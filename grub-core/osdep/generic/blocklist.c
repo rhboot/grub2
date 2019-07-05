dabdfa1c6a80639197d05f683a445fa8615517fe
David Michael <fedora.dm0@gmail.com>
2019-07-05 07:45:59 -0400

    verifiers: Blocklist fallout cleanup
    Blocklist fallout cleanup after commit 5c6f9bc15 (generic/blocklist: Fix
    implicit declaration of function grub_file_filter_disable_compression()).
    
    Signed-off-by: David Michael <fedora.dm0@gmail.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- grub-core/osdep/generic/blocklist.c
+++ grub-core/osdep/generic/blocklist.c
@@ -59,7 +59,7 @@ grub_install_get_blocklist (grub_device_t root_dev,
 
       grub_disk_cache_invalidate_all ();
 
-      file = grub_file_open (core_path_dev, GRUB_FILE_TYPE_NONE | FILE_TYPE_NO_DECOMPRESS);
+      file = grub_file_open (core_path_dev, GRUB_FILE_TYPE_NONE | GRUB_FILE_TYPE_NO_DECOMPRESS);
       if (file)
 	{
 	  if (grub_file_size (file) != core_size)
@@ -116,7 +116,7 @@ grub_install_get_blocklist (grub_device_t root_dev,
 
   grub_file_t file;
   /* Now read the core image to determine where the sectors are.  */
-  file = grub_file_open (core_path_dev, GRUB_FILE_TYPE_NONE | FILE_TYPE_NO_DECOMPRESS);
+  file = grub_file_open (core_path_dev, GRUB_FILE_TYPE_NONE | GRUB_FILE_TYPE_NO_DECOMPRESS);
   if (! file)
     grub_util_error ("%s", grub_errmsg);
 
