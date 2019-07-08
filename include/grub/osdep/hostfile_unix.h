048f9e69b704438b820d8f57b329ca136d5fd7cf
Peter Jones <pjones@redhat.com>
2019-07-08 21:46:52 +0200

    Add grub_util_readlink()
    Add grub_util_readlink().  This requires pulling in stat and readlink from
    gnulib, which pulls in stat and related headers, but after that the
    implementation is straightforward.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>

--- include/grub/osdep/hostfile_unix.h
+++ include/grub/osdep/hostfile_unix.h
@@ -71,6 +71,12 @@ grub_util_rename (const char *from, const char *to)
   return rename (from, to);
 }
 
+static inline ssize_t
+grub_util_readlink (const char *name, char *buf, size_t bufsize)
+{
+  return readlink(name, buf, bufsize);
+}
+
 #define grub_util_mkdir(a) mkdir ((a), 0755)
 
 #if defined (__NetBSD__)
