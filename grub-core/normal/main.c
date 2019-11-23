3864f917ba3594e946d84e91e7f8d6570a13e25e
Renaud Métrich <rmetrich@redhat.com>
2019-11-23 16:23:54 +0100

    Don't clear screen when debugging is enabled
    Signed-off-by: Renaud Métrich <rmetrich@redhat.com>
    [rharwood@redhat.com: rebase fuzz]
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>

--- grub-core/normal/main.c
+++ grub-core/normal/main.c
@@ -216,7 +216,8 @@ grub_normal_init_page (struct grub_term_output *term,
   grub_uint32_t *unicode_msg;
   grub_uint32_t *last_position;
 
-  grub_term_cls (term);
+  if (! grub_debug_is_enabled ())
+    grub_term_cls (term);
 
   msg_formatted = grub_xasprintf (_("GRUB version %s"), PACKAGE_VERSION);
   if (!msg_formatted)
