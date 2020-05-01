8ac4a9f94d7db051eab1910ac0d9ec21594e6e64
Daniel Axtens <dja@axtens.net>
2020-05-01 17:12:23 +1000

    libtasn1: disable code not needed in grub
    We don't expect to be able to write ASN.1, only read it,
    so we can disable some code.
    
    Do that with #if 0/#endif, rather than deletion. This means
    that the difference between upstream and grub is smaller,
    which should make updating libtasn1 easier in the future.
    
    With these exclusions we also avoid the need for minmax.h,
    which is convenient because it means we don't have to
    import it from gnulib.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>

--- grub-core/lib/libtasn1/lib/decoding.c
+++ grub-core/lib/libtasn1/lib/decoding.c
@@ -1613,6 +1613,7 @@ asn1_der_decoding (asn1_node * element, const void *ider, int ider_len,
   return asn1_der_decoding2 (element, ider, &ider_len, 0, errorDescription);
 }
 
+#if 0
 /**
  * asn1_der_decoding_element:
  * @structure: pointer to an ASN1 structure
@@ -1643,6 +1644,7 @@ asn1_der_decoding_element (asn1_node * structure, const char *elementName,
 {
   return asn1_der_decoding(structure, ider, len, errorDescription);
 }
+#endif
 
 /**
  * asn1_der_decoding_startEnd:
