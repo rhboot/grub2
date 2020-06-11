fb8b576b863448bba87b912782d0fa486bba110b
Rashmica Gupta <rashmica.g@gmail.com>
2020-06-11 11:26:23 +1000

    Add suport for signing grub with an appended signature
    Add infrastructure to allow firmware to verify the integrity of grub
    by use of a Linux-kernel-module-style appended signature. We initially
    target powerpc-ieee1275, but the code should be extensible to other
    platforms.
    
    Usually these signatures are appended to a file without modifying the
    ELF file itself. (This is what the 'sign-file' tool does, for example.)
    The verifier loads the signed file from the file system and looks at the
    end of the file for the appended signature. However, on powerpc-ieee1275
    platforms, the bootloader is often stored directly in the PReP partition
    as raw bytes without a file-system. This makes determining the location
    of an appended signature more difficult.
    
    To address this, we add a new ELF note.
    
    The name field of shall be the string "Appended-Signature", zero-padded
    to 4 byte alignment. The type field shall be 0x41536967 (the ASCII values
    for the string "ASig"). It must be the final section in the ELF binary.
    
    The description shall contain the appended signature structure as defined
    by the Linux kernel. The description will also be padded to be a multiple
    of 4 bytes. The padding shall be added before the appended signature
    structure (not at the end) so that the final bytes of a signed ELF file
    are the appended signature magic.
    
    A subsequent patch documents how to create a grub core.img validly signed
    under this scheme.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>
    Signed-off-by: Rashmica Gupta <rashmica.g@gmail.com>

--- util/grub-mkimagexx.c
+++ util/grub-mkimagexx.c
@@ -85,6 +85,15 @@ struct grub_ieee1275_note
   struct grub_ieee1275_note_desc descriptor;
 };
 
+#define GRUB_APPENDED_SIGNATURE_NOTE_NAME "Appended-Signature"
+#define GRUB_APPENDED_SIGNATURE_NOTE_TYPE 0x41536967 /* "ASig" */
+
+struct grub_appended_signature_note
+{
+  Elf32_Nhdr header;
+  char name[ALIGN_UP(sizeof (GRUB_APPENDED_SIGNATURE_NOTE_NAME), 4)];
+};
+
 #define GRUB_XEN_NOTE_NAME "Xen"
 
 struct fixup_block_list
@@ -208,7 +217,7 @@ grub_arm_reloc_jump24 (grub_uint32_t *target, Elf32_Addr sym_addr)
 
 void
 SUFFIX (grub_mkimage_generate_elf) (const struct grub_install_image_target_desc *image_target,
-				    int note, char **core_img, size_t *core_size,
+				    int note, size_t appsig_size, char **core_img, size_t *core_size,
 				    Elf_Addr target_addr,
 				    struct grub_mkimage_layout *layout)
 {
@@ -221,6 +230,12 @@ SUFFIX (grub_mkimage_generate_elf) (const struct grub_install_image_target_desc 
   int phnum = 1;
   int shnum = 4;
   int string_size = sizeof (".text") + sizeof ("mods") + 1;
+
+  if (appsig_size)
+    {
+      phnum++;
+      footer_size += ALIGN_UP(sizeof (struct grub_appended_signature_note) + appsig_size, 4);
+    }
 
   if (image_target->id != IMAGE_LOONGSON_ELF)
     phnum += 2;
@@ -484,6 +499,28 @@ SUFFIX (grub_mkimage_generate_elf) (const struct grub_install_image_target_desc 
       phdr->p_memsz = 0;
       phdr->p_offset = grub_host_to_target32 (header_size + program_size);
     }
+
+  if (appsig_size) {
+    int note_size = ALIGN_UP(sizeof (struct grub_appended_signature_note) + appsig_size, 4);
+    struct grub_appended_signature_note *note_ptr = (struct grub_appended_signature_note *)
+      (elf_img + program_size + header_size + (note ? sizeof (struct grub_ieee1275_note) : 0));
+
+    note_ptr->header.n_namesz = grub_host_to_target32 (sizeof (GRUB_APPENDED_SIGNATURE_NOTE_NAME));
+    /* needs to sit at the end, so we round this up and sign some zero padding */
+    note_ptr->header.n_descsz = grub_host_to_target32 (ALIGN_UP(appsig_size, 4));
+    note_ptr->header.n_type = grub_host_to_target32 (GRUB_APPENDED_SIGNATURE_NOTE_TYPE);
+    strcpy (note_ptr->name, GRUB_APPENDED_SIGNATURE_NOTE_NAME);
+
+    phdr++;
+    phdr->p_type = grub_host_to_target32 (PT_NOTE);
+    phdr->p_flags = grub_host_to_target32 (PF_R);
+    phdr->p_align = grub_host_to_target32 (image_target->voidp_sizeof);
+    phdr->p_vaddr = 0;
+    phdr->p_paddr = 0;
+    phdr->p_filesz = grub_host_to_target32 (note_size);
+    phdr->p_memsz = 0;
+    phdr->p_offset = grub_host_to_target32 (header_size + program_size + (note ? sizeof (struct grub_ieee1275_note) : 0));
+  }
 
   {
     char *str_start = (elf_img + sizeof (*ehdr) + phnum * sizeof (*phdr)
