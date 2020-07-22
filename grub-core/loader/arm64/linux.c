170ec68a6d9c2b9d01cd95a637bf88d2f12c34c6
Dimitri John Ledkov <xnox@ubuntu.com>
2020-07-22 11:31:43 +0100

    linuxefi: fail kernel validation without shim protocol.
    If certificates that signed grub are installed into db, grub can be
    booted directly. It will then boot any kernel without signature
    validation. The booted kernel will think it was booted in secureboot
    mode and will implement lockdown, yet it could have been tampered.
    
    This version of the patch skips calling verification, when booted
    without secureboot. And is indented with gnu ident.
    
    CVE-2020-15705
    
    Reported-by: Mathieu Trudel-Lapierre <cyphermox@ubuntu.com>
    Signed-off-by: Dimitri John Ledkov <xnox@ubuntu.com>

--- grub-core/loader/arm64/linux.c
+++ grub-core/loader/arm64/linux.c
@@ -34,6 +34,7 @@
 #include <grub/i18n.h>
 #include <grub/lib/cmdline.h>
 #include <grub/verify.h>
+#include <grub/efi/sb.h>
 
 GRUB_MOD_LICENSE ("GPLv3+");
 
@@ -383,11 +384,15 @@ grub_cmd_linux (grub_command_t cmd __attribute__ ((unused)),
 
   grub_dprintf ("linux", "kernel @ %p\n", kernel_addr);
 
-  rc = grub_linuxefi_secure_validate (kernel_addr, kernel_size);
-  if (rc < 0)
+  if (grub_efi_secure_boot ())
     {
-      grub_error (GRUB_ERR_INVALID_COMMAND, N_("%s has invalid signature"), argv[0]);
-      goto fail;
+      rc = grub_linuxefi_secure_validate (kernel_addr, kernel_size);
+      if (rc <= 0)
+	{
+	  grub_error (GRUB_ERR_INVALID_COMMAND,
+		      N_("%s has invalid signature"), argv[0]);
+	  goto fail;
+	}
     }
 
   pe = (void *)((unsigned long)kernel_addr + lh.hdr_offset);
--- grub-core/loader/efi/chainloader.c
+++ grub-core/loader/efi/chainloader.c
@@ -1083,6 +1083,7 @@ grub_cmd_chainloader (grub_command_t cmd __attribute__ ((unused)),
 
       return 0;
     }
+  // -1 fall-through to fail
 
 fail:
   if (dev)
