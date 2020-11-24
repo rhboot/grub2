d91f98d4f4a96d8b1bc3f84882c062a67e216cde
Darren Kenny <darren.kenny@oracle.com>
2020-11-24 18:04:22 +0000

    gnulib/regcomp: Fix uninitialized re_token
    This issue has been fixed in the latest version of gnulib, so to
    maintain consistency, I've backported that change rather than doing
    something different.
    
    Fixes: CID 73828
    
    Signed-off-by: Darren Kenny <darren.kenny@oracle.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- grub-core/gnulib/regcomp.c
+++ grub-core/gnulib/regcomp.c
@@ -3790,8 +3790,7 @@ static bin_tree_t *
 create_tree (re_dfa_t *dfa, bin_tree_t *left, bin_tree_t *right,
 	     re_token_type_t type)
 {
-  re_token_t t;
-  t.type = type;
+  re_token_t t = { .type = type };
   return create_token_tree (dfa, left, right, &t);
 }
 
