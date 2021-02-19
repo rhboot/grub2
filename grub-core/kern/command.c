5311766df2a22d0656c7ed8412a44d15a204a64e
Javier Martinez Canillas <javierm@redhat.com>
2021-02-19 10:33:54 +0100

    kern: Add lockdown support
    When the GRUB starts on a secure boot platform, some commands can be
    used to subvert the protections provided by the verification mechanism and
    could lead to booting untrusted system.
    
    To prevent that situation, allow GRUB to be locked down. That way the code
    may check if GRUB has been locked down and further restrict the commands
    that are registered or what subset of their functionality could be used.
    
    The lockdown support adds the following components:
    
    * The grub_lockdown() function which can be used to lockdown GRUB if,
      e.g., UEFI Secure Boot is enabled.
    
    * The grub_is_lockdown() function which can be used to check if the GRUB
      was locked down.
    
    * A verifier that flags OS kernels, the GRUB modules, Device Trees and ACPI
      tables as GRUB_VERIFY_FLAGS_DEFER_AUTH to defer verification to other
      verifiers. These files are only successfully verified if another registered
      verifier returns success. Otherwise, the whole verification process fails.
    
      For example, PE/COFF binaries verification can be done by the shim_lock
      verifier which validates the signatures using the shim_lock protocol.
      However, the verification is not deferred directly to the shim_lock verifier.
      The shim_lock verifier is hooked into the verification process instead.
    
    * A set of grub_{command,extcmd}_lockdown functions that can be used by
      code registering command handlers, to only register unsafe commands if
      the GRUB has not been locked down.
    
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- grub-core/kern/command.c
+++ grub-core/kern/command.c
@@ -17,6 +17,7 @@
  *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <grub/lockdown.h>
 #include <grub/mm.h>
 #include <grub/command.h>
 
@@ -75,6 +76,29 @@ grub_register_command_prio (const char *name,
     cmd->prio |= GRUB_COMMAND_FLAG_ACTIVE;
 
   return cmd;
+}
+
+static grub_err_t
+grub_cmd_lockdown (grub_command_t cmd __attribute__ ((unused)),
+                   int argc __attribute__ ((unused)),
+                   char **argv __attribute__ ((unused)))
+
+{
+  return grub_error (GRUB_ERR_ACCESS_DENIED,
+                     N_("%s: the command is not allowed when lockdown is enforced"),
+                     cmd->name);
+}
+
+grub_command_t
+grub_register_command_lockdown (const char *name,
+                                grub_command_func_t func,
+                                const char *summary,
+                                const char *description)
+{
+  if (grub_is_lockdown () == GRUB_LOCKDOWN_ENABLED)
+    func = grub_cmd_lockdown;
+
+  return grub_register_command_prio (name, func, summary, description, 0);
 }
 
 void
