coreos/grub

commit 9acdcbf3254277b44d4558cfe73fde272b7d6e91

use single quotes in menuentry setparams command

grub-core/commands/menuentry.c
@@ -206,20 +206,6 @@ setparams_prefix (int argc, char **args)
   char *p;
   char *result;
   grub_size_t len = 10;
-  static const char *escape_characters = "\"\\";
-
-  auto char *strescpy (char *, const char *, const char *);
-  char * strescpy (char *d, const char *s, const char *escapes)
-  {
-    while (*s)
-      {
-	if (grub_strchr (escapes, *s))
-	  *d++ = '\\';
-	*d++ = *s++;
-      }
-    *d = '\0';
-    return d;
-  }
 
   /* Count resulting string length */
   for (i = 0; i < argc; i++)
@@ -227,7 +213,7 @@ setparams_prefix (int argc, char **args)
       len += 3; /* 3 = 1 space + 2 quotes */
       p = args[i];
       while (*p)
-	len += grub_strchr (escape_characters, *p++) ? 2 : 1;
+	len += (*p++ == '\'' ? 3 : 1);
     }
 
   result = grub_malloc (len + 2);
@@ -240,9 +226,18 @@ setparams_prefix (int argc, char **args)
   for (j = 0; j < argc; j++)
     {
       result[i++] = ' ';
-      result[i++] = '"';
-      i = strescpy (result + i, args[j], escape_characters) - result;
-      result[i++] = '"';
+      result[i++] = '\'';
+      p = args[j];
+      while (*p) {
+	result[i++] = *p;
+	if (*p == '\'') {
+	  result[i++] = '\\';
+	  result[i++] = '\'';
+	  result[i++] = '\'';
+	}
+	p++;
+      }
+      result[i++] = '\'';
     }
   result[i++] = '\n';
   result[i] = '\0';

rhboot/grub2

commit c1fc29d5c68ca9a8ffffe9181bf2bc9775270de8

commands/menuentry: Fix quoting in setparams_prefix()
Commit 9acdcbf32542 (use single quotes in menuentry setparams command)
says that expressing a quoted single quote will require 3 characters. It
actually requires (and always did require!) 4 characters:

  str: a'b => a'\''b
  len:  3  => 6 (2 for the letters + 4 for the quote)

This leads to not allocating enough memory and thus out of bounds writes
that have been observed to cause heap corruption.

Allocate 4 bytes for each single quote.

Commit 22e7dbb2bb81 (Fix quoting in legacy parser.) does the same
quoting, but it adds 3 as extra overhead on top of the single byte that
the quote already needs. So it's correct.

Fixes: CVE-2021-20233
Fixes: 9acdcbf32542 (use single quotes in menuentry setparams command)

Signed-off-by: Daniel Axtens <dja@axtens.net>
Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

grub-core/commands/menuentry.c
@@ -239,7 +239,7 @@ setparams_prefix (int argc, char **args)
       len += 3; /* 3 = 1 space + 2 quotes */
       p = args[i];
       while (*p)
-	len += (*p++ == '\'' ? 3 : 1);
+	len += (*p++ == '\'' ? 4 : 1);
     }
 
   result = grub_malloc (len + 2);