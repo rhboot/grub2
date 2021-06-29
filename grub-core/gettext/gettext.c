12f977df14ede7248a39d4463ada7f903a013508
Paulo Flabiano Smorigo <pfsmorigo@br.ibm.com>
2021-06-29 13:17:42 +0200

    Suppress gettext error message
    Colin Watson's patch from comment #11 on the upstream bug:
    https://savannah.gnu.org/bugs/?35880#comment11
    
    Resolves: rhbz#1592124
    
    Signed-off-by: Paulo Flabiano Smorigo <pfsmorigo@br.ibm.com>

--- grub-core/gettext/gettext.c
+++ grub-core/gettext/gettext.c
@@ -424,6 +424,13 @@ grub_gettext_init_ext (struct grub_gettext_context *ctx,
       grub_free (lang);
     }
 
+  /* If no translations are available, fall back to untranslated text. */
+  if (err == GRUB_ERR_FILE_NOT_FOUND)
+    {
+      grub_errno = GRUB_ERR_NONE;
+      return 0;
+    }
+
   if (locale[0] == 'e' && locale[1] == 'n'
       && (locale[2] == '\0' || locale[2] == '_'))
     grub_errno = err = GRUB_ERR_NONE;
