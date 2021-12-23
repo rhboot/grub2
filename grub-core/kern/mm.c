coreos/grub

commit 45bf8b3a75492085f2a7a0563b301caabfbeb63d

* grub-core/kern/mm.c (grub_real_malloc): Decrease cut-off of moving the 	pointer to 32K. This is the size of cache element which is the most 	common allocation >1K. This way the pointer is always around blocks 	of 32K and so we keep performance while decreasing fragmentation.

grub-core/kern/mm.c
@@ -299,7 +299,7 @@ grub_real_malloc (grub_mm_header_t *first, grub_size_t n, grub_size_t align)
 	     This will have side effect of fragmenting memory as small
 	     pieces before this will be un-used.  */
 	  /* So do it only for chunks under 64K.  */
-	  if (n < (0x10000 >> GRUB_MM_ALIGN_LOG2)
+	  if (n < (0x8000 >> GRUB_MM_ALIGN_LOG2)
 	      || *first == p)
 	    *first = q;
 

rhboot/grub2

commit a847895a8d000bdf27ad4d4326f883a0eed769ca

mm: grub_real_malloc(): Make small allocs comment match code
Small allocations move the region's *first pointer. The comment
says that this happens for allocations under 64K. The code says
it's for allocations under 32K. Commit 45bf8b3a7549 changed the
code intentionally: make the comment match.

Fixes: 45bf8b3a7549 (* grub-core/kern/mm.c (grub_real_malloc): Decrease cut-off of moving the)

Signed-off-by: Daniel Axtens <dja@axtens.net>
Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

grub-core/kern/mm.c
@@ -306,7 +306,7 @@ grub_real_malloc (grub_mm_header_t *first, grub_size_t n, grub_size_t align)
 	  /* Mark find as a start marker for next allocation to fasten it.
 	     This will have side effect of fragmenting memory as small
 	     pieces before this will be un-used.  */
-	  /* So do it only for chunks under 64K.  */
+	  /* So do it only for chunks under 32K.  */
 	  if (n < (0x8000 >> GRUB_MM_ALIGN_LOG2)
 	      || *first == cur)
 	    *first = prev;