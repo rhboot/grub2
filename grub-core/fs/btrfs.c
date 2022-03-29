e447f886cea5bd0857d24c3156428cef85b6987b
Darren Kenny <darren.kenny@oracle.com>
2022-03-29 10:49:56 +0000

    fs/btrfs: Fix several fuzz issues with invalid dir item sizing
    According to the btrfs code in Linux, the structure of a directory item
    leaf should be of the form:
    
      |struct btrfs_dir_item|name|data|
    
    in GRUB the name len and data len are in the grub_btrfs_dir_item
    structure's n and m fields respectively.
    
    The combined size of the structure, name and data should be less than
    the allocated memory, a difference to the Linux kernel's struct
    btrfs_dir_item is that the grub_btrfs_dir_item has an extra field for
    where the name is stored, so we adjust for that too.
    
    Signed-off-by: Darren Kenny <darren.kenny@oracle.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>
    (cherry picked from commit 6d3f06c0b6a8992b9b1bb0e62af93ac5ff2781f0)
    [rharwood: we've an extra variable here]
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>

--- grub-core/fs/btrfs.c
+++ grub-core/fs/btrfs.c
@@ -2254,6 +2254,7 @@ grub_btrfs_dir (grub_device_t device, const char *path,
   grub_uint64_t tree;
   grub_uint8_t type;
   char *new_path = NULL;
+  grub_size_t est_size = 0;
 
   if (!data)
     return grub_errno;
@@ -2320,6 +2321,18 @@ grub_btrfs_dir (grub_device_t device, const char *path,
 	  break;
 	}
 
+      if (direl == NULL ||
+	  grub_add (grub_le_to_cpu16 (direl->n),
+		    grub_le_to_cpu16 (direl->m), &est_size) ||
+	  grub_add (est_size, sizeof (*direl), &est_size) ||
+	  grub_sub (est_size, sizeof (direl->name), &est_size) ||
+	  est_size > allocated)
+       {
+         grub_errno = GRUB_ERR_OUT_OF_RANGE;
+         r = -grub_errno;
+         goto out;
+       }
+
       for (cdirel = direl;
 	   (grub_uint8_t *) cdirel - (grub_uint8_t *) direl
 	   < (grub_ssize_t) elemsize;
@@ -2330,6 +2343,19 @@ grub_btrfs_dir (grub_device_t device, const char *path,
 	  char c;
 	  struct grub_btrfs_inode inode;
 	  struct grub_dirhook_info info;
+
+	  if (cdirel == NULL ||
+	      grub_add (grub_le_to_cpu16 (cdirel->n),
+			grub_le_to_cpu16 (cdirel->m), &est_size) ||
+	      grub_add (est_size, sizeof (*cdirel), &est_size) ||
+	      grub_sub (est_size, sizeof (cdirel->name), &est_size) ||
+	      est_size > allocated)
+	   {
+	     grub_errno = GRUB_ERR_OUT_OF_RANGE;
+	     r = -grub_errno;
+	     goto out;
+	   }
+
 	  err = grub_btrfs_read_inode (data, &inode, cdirel->key.object_id,
 				       tree);
 	  grub_memset (&info, 0, sizeof (info));
