rhboot/grub2

commit e981b0a248ba62683ff574863656d7b0aae7fa12

osdep/hurd/getroot: Use "part:" qualifier
When using userland drivers such as rumpdisk, we'd rather make ext2fs use
parted-based libstore partitioning support. That can be used for kernelland
drivers as well, so we can just make GRUB always use the "part:" qualifier
to switch ext2fs to it.

grub_util_find_hurd_root_device() then has to understand this syntax and
translate it into the /dev/ entry name.

Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>
Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

grub-core/osdep/hurd/getroot.c
@@ -109,6 +109,16 @@ grub_util_find_hurd_root_device (const char *path)
     grub_util_error (_("translator `%s' for path `%s' is given only options, "
                        "cannot find device part"), argz, path);
 
+  int part = -1;
+  if (strncmp (name, "part:", sizeof ("part:") - 1) == 0)
+    {
+      char *next = strchr (name + sizeof ("part:") - 1, ':');
+      if (next)
+        {
+          part = atoi (name + sizeof ("part:") - 1);
+          name = next + 1;
+        }
+    }
   if (strncmp (name, "device:", sizeof ("device:") - 1) == 0)
     {
       char *dev_name = name + sizeof ("device:") - 1;
@@ -127,10 +137,10 @@ grub_util_find_hurd_root_device (const char *path)
             dev_name = next_name + 1;
         }
 
-      size = sizeof ("/dev/") - 1 + strlen (dev_name) + 1;
-      ret = xmalloc (size);
-      next = stpncpy (ret, "/dev/", size);
-      stpncpy (next, dev_name, size - (next - ret));
+      if (part >= 0)
+        ret = xasprintf("/dev/%ss%u", dev_name, part);
+      else
+        ret = xasprintf("/dev/%s", dev_name);
     }
   else if (!strncmp (name, "file:", sizeof ("file:") - 1))
     ret = strdup (name + sizeof ("file:") - 1);

util/grub.d/10_hurd.in
@@ -128,9 +128,11 @@ EOF
   else
       opts=
   fi
+  device=device:${GRUB_DEVICE#/dev/}
+  device=$(echo "$device" | sed -e 's/^device:\(.*[0-9]\+\)s\([0-9]\+\)$/part:\2:device:\1'/)
   sed "s/^/$submenu_indentation/" << EOF
 	echo		'$(echo "$message" | grub_quote)'
-	multiboot	${kernel} root=device:${GRUB_DEVICE#/dev/} $opts ${GRUB_CMDLINE_GNUMACH}
+	multiboot	${kernel} root=$device $opts ${GRUB_CMDLINE_GNUMACH}
 EOF
 
   if [ x$type != xrecovery ] ; then

commit 02478007f38424f8434dd130c57df0b3a998346c

osdep/hurd/getroot: Remove unused variables in grub_util_find_hurd_root_device()
Found during a test build on Debian/hurd-i386 with --disable-werror enabled:

  In file included from grub-core/osdep/getroot.c:12:
  grub-core/osdep/hurd/getroot.c: In function ‘grub_util_find_hurd_root_device’:
  grub-core/osdep/hurd/getroot.c:126:13: error: unused variable ‘next’ [-Werror=unused-variable]
    126 |       char *next;
        |             ^~~~
  grub-core/osdep/hurd/getroot.c:125:14: error: unused variable ‘size’ [-Werror=unused-variable]
    125 |       size_t size;
        |              ^~~~

Fixes: e981b0a24 (osdep/hurd/getroot: Use "part:" qualifier)

Signed-off-by: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
Reviewed-by: Samuel Thibault <samuel.thibault@ens-lyon.org>
Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

grub-core/osdep/hurd/getroot.c
@@ -122,8 +122,6 @@ grub_util_find_hurd_root_device (const char *path)
   if (strncmp (name, "device:", sizeof ("device:") - 1) == 0)
     {
       char *dev_name = name + sizeof ("device:") - 1;
-      size_t size;
-      char *next;
 
       if (dev_name[0] == '@')
         {