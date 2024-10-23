f97d4618a57e1969e8ba51869b8cd250ff24116c
Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
2024-10-23 17:54:32 +0530

    grub-mkimage: Create new ELF note for SBAT
    In order to store the SBAT data we create a new ELF note. The string
    ".sbat", zero-padded to 4 byte alignment, shall be entered in the name
    field. The string "SBAT"'s ASCII values, 0x53424154, should be entered
    in the type field.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>
    Signed-off-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- util/mkimage.c
+++ util/mkimage.c
@@ -1814,6 +1814,7 @@ grub_install_generate_image (const char *dir, const char *prefix,
     case IMAGE_I386_IEEE1275:
       {
 	grub_uint64_t target_addr;
+	char *sbat = NULL;
 	if (image_target->id == IMAGE_LOONGSON_ELF)
 	  {
 	    if (comp == GRUB_COMPRESSION_NONE)
@@ -1825,10 +1826,10 @@ grub_install_generate_image (const char *dir, const char *prefix,
 	else
 	  target_addr = image_target->link_addr;
 	if (image_target->voidp_sizeof == 4)
-	  grub_mkimage_generate_elf32 (image_target, note, &core_img, &core_size,
+	  grub_mkimage_generate_elf32 (image_target, note, sbat, &core_img, &core_size,
 				       target_addr, &layout);
 	else
-	  grub_mkimage_generate_elf64 (image_target, note, &core_img, &core_size,
+	  grub_mkimage_generate_elf64 (image_target, note, sbat, &core_img, &core_size,
 				       target_addr, &layout);
       }
       break;
