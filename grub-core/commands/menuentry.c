9d056f48e3ea3d0458965fb817fc2b6b50ed2e81
Peter Jones <pjones@redhat.com>
2025-08-12 03:45:33 +0000

    blsuki: Add blscfg command to parse Boot Loader Specification snippets
    The BootLoaderSpec (BLS) defines a scheme where different bootloaders can
    share a format for boot items and a configuration directory that accepts
    these common configurations as drop-in files.
    
    The BLS Specification: https://uapi-group.org/specifications/specs/boot_loader_specification/
    
    Signed-off-by: Peter Jones <pjones@redhat.com>
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>
    Signed-off-by: Will Thompson <wjt@endlessm.com>
    Signed-off-by: Alec Brown <alec.r.brown@oracle.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- grub-core/commands/menuentry.c
+++ grub-core/commands/menuentry.c
@@ -78,7 +78,7 @@ grub_normal_add_menu_entry (int argc, const char **args,
 			    char **classes, const char *id,
 			    const char *users, const char *hotkey,
 			    const char *prefix, const char *sourcecode,
-			    int submenu, int *index, struct bls_entry *bls)
+			    int submenu, int *index, struct bls_entry *bls, grub_blsuki_entry_t *blsuki)
 {
   int menu_hotkey = 0;
   char **menu_args = NULL;
@@ -196,6 +196,7 @@ grub_normal_add_menu_entry (int argc, const char **args,
   (*last)->sourcecode = menu_sourcecode;
   (*last)->submenu = submenu;
   (*last)->bls = bls;
+  (*last)->blsuki = blsuki;
 
   menu->size++;
   if (index)
@@ -297,7 +298,7 @@ grub_cmd_menuentry (grub_extcmd_context_t ctxt, int argc, char **args)
 				       ctxt->state[2].arg, 0,
 				       ctxt->state[3].arg,
 				       ctxt->extcmd->cmd->name[0] == 's',
-				       NULL, NULL);
+				       NULL, NULL, NULL);
 
   src = args[argc - 1];
   args[argc - 1] = NULL;
@@ -315,7 +316,7 @@ grub_cmd_menuentry (grub_extcmd_context_t ctxt, int argc, char **args)
 				  users,
 				  ctxt->state[2].arg, prefix, src + 1,
 				  ctxt->extcmd->cmd->name[0] == 's', NULL,
-				  NULL);
+				  NULL, NULL);
 
   src[len - 1] = ch;
   args[argc - 1] = src;
