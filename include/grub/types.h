053fb961549bfda15dd1fbeff588ef7c3f4ffdc7
Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
2025-10-06 12:54:58 +0530

    appended signatures: Create db and dbx lists
    If secure boot is enabled with static key management mode, the trusted
    certificates will be extracted from the GRUB ELF Note and added to db list.
    
    If secure boot is enabled with dynamic key management mode, the trusted
    certificates and certificate/binary hash will be extracted from the PKS
    and added to db list. The distrusted certificates, certificate/binary hash
    are read from the PKS and added to dbx list. Both dbx and db lists usage is
    added by a subsequent patch.
    
    Note:
    - If db does not exist in the PKS storage, then read the static keys as a db
      default keys from the GRUB ELF Note and add them into the db list.
    - If the certificate or the certificate hash exists in the dbx list, then do not
      add that certificate/certificate hash to the db list.
    
    Signed-off-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
    Reviewed-by: Avnish Chouhan <avnish@linux.ibm.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- include/grub/types.h
+++ include/grub/types.h
@@ -364,6 +364,8 @@ struct grub_guid
 } __attribute__ ((aligned(4)));
 typedef struct grub_guid grub_guid_t;
 
+#define GRUB_GUID_SIZE    (sizeof (grub_guid_t))
+
 struct grub_packed_guid
 {
   grub_uint32_t data1;
@@ -372,5 +374,7 @@ struct grub_packed_guid
   grub_uint8_t data4[8];
 } GRUB_PACKED;
 typedef struct grub_packed_guid grub_packed_guid_t;
+
+#define GRUB_PACKED_GUID_SIZE    (sizeof (grub_packed_guid_t))
 
 #endif /* ! GRUB_TYPES_HEADER */
